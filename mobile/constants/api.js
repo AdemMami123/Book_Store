281e21be92d919fb0db0e8dadd902e4ad0e2db7a
Adem Mami <adem.mami@itbs.tn>
2025-05-29 04:17:07 +0100

    fixed some bugs

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,4 +1,4 @@
 // IMPORTANT: Choose the correct API URL based on your environment
 // For Android Emulator - Use 10.0.2.2 instead of localhost
-//export const API_URL = "http://10.0.2.2:3000";
-export const API_URL = "https://book-store-vheh.onrender.com";
+export const API_URL = "http://10.0.2.2:3000";
+//export const API_URL = "https://book-store-vheh.onrender.com";
